50690325e6960712b6918f86c755f2b904d6f84d
svikas641 <svikas641@gmail.com>
2020-07-26 20:53:05 +0530

    Updates

---
+++ precache-manifest.780c7ea1564c1d0e04d6b62e652690ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7127383d5aa511a7ecfb485a8b401b4",
+    "url": "/react-asignment/index.html"
+  },
+  {
+    "revision": "95893ec23f8ef01ab4a7",
+    "url": "/react-asignment/static/css/main.16587cbf.chunk.css"
+  },
+  {
+    "revision": "5cf24cc498945b5915c1",
+    "url": "/react-asignment/static/js/2.c10d4f0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-asignment/static/js/2.c10d4f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95893ec23f8ef01ab4a7",
+    "url": "/react-asignment/static/js/main.534cda9d.chunk.js"
+  },
+  {
+    "revision": "f1dcb9c8602de8b12eba",
+    "url": "/react-asignment/static/js/runtime-main.82635f5d.js"
+  }
+]);
